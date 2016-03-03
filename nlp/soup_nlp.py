658d88b4f447aded43d7bee3f8bd54e737489412
Pranabesh Sinha <pranabesh.sinha@gmail.com>
2016-03-02 18:09:56 -0800

    extracting text is buggy

--- nlp/soup_nlp.py
+++ nlp/soup_nlp.py
@@ -2,28 +2,9 @@ import re
 from bs4 import BeautifulSoup, NavigableString
 from urllib.request import urlopen
 import nltk
-
-
-def soupify(url):
-    page = urlopen(url)
-    soup = BeautifulSoup(page.read())
-    # get the title
-    title_tag = soup.html.head.title.string
-    # print all contents in all paragraph
-    all_para = soup.find_all('p')
-    for para in all_para:
-        try:
-            line = " ".join(para.contents)
-            line = re.sub('[n]','', line)
-            if len(line) >= 10:
-                print(line)
-        except:
-            pass
-
-    # everything in a body
-    # for i in soup.body:
-    #     print(i)
-
+from nltk.corpus import stopwords
+import operator
+import sys
 
 def get_text_from_url(url):
     page = urlopen(url).read().decode('utf8')
@@ -54,7 +35,41 @@ def extract_text_from_tag(tag):
                 pass
 
 
+def get_top_five(text_data):
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    text = ''.join(text_data)
+    sentences = sent_detector.tokenize(text.lower().strip())
+    stopped_sent = []
+    ngram = []
+    pattern=re.compile("[^\w']")
+    # remove stopwords
+    for sentence in sentences:
+        # print sentence
+        # clear up punctuations
+        # print(sentence)
+        cleaned_sentence = str(pattern.sub(' ', sentence))
+        # remove stopwords
+        words = [word for word in cleaned_sentence.split() if word not in stopwords.words('english')]
+        joint = ""
+        # if nothing is left after removing stopwords don't append
+        if len(words) > 0:
+            joint = " ".join(words)
+        # print (joint)
+        stopped_sent.append(joint)
+    # create Ngrams
+    for sentence in stopped_sent:
+        ngram += nltk.trigrams(sentence.split())
+    flat_ngram_list = [item for sublist in ngram for item in sublist]
+    fdist = nltk.FreqDist(flat_ngram_list)
+    sorted_x = sorted(fdist.items(), key=operator.itemgetter(1), reverse=True)
+    for k in sorted_x[:7]:
+        print ("".join(k[0]) + ":" + str(k[1]))
+
+
 if __name__ == '__main__':
-    text_data = get_text_from_url('https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm')
-    # text_data = get_text_from_url('http://prnbs.github.io/')
+    # text_data = get_text_from_url('https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm')
+    # print(sys.argv[1])
+    text_data = get_text_from_url('http://ssarangi.github.io/Luajit-For-Python/')
     print(text_data)
+    ngram = get_top_five(text_data)
+    # print(text_data)
